80fe26feb553fce520ed02fa07d033370e8ea297
sourabhk25 <sourabhkulkarni258@gmail.com>
2021-10-27 19:43:06 -0700

    frontend URL added

--- src/main/java/com/sourkul/config/AppConfig.java
+++ src/main/java/com/sourkul/config/AppConfig.java
@@ -42,7 +42,7 @@ public class AppConfig {
 								"http://localhost:3000", 
 								"http://localhost:4000",
 								"http://localhost:4200",
-								"https://shopwithzosh.vercel.app"
+								"https://serene-styles.vercel.app/"
 								
 							)
 						);
